41a6d42b04dfcaf7e990ddc926c8b7f687c3a8bc
krishnansai <krishnansai71@gmail.com>
2022-01-26 12:08:39 +0530

    k

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,27 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import UserMain from "./components/user/UserMain";
 function App() {
   return (
-    <>
+    <div className="index">
     <Router>
-      <Link to="/admin">Login</Link>
+      <Link to="/admin">Admin_Login</Link>
+      <Link to="/user">user_Login</Link>
+
     <div className="App">
       <Switch>
         <Route path="/admin">
           <AdminMain />
         </Route>
+        
+        <Route path="/user">
+          <UserMain/>
+        </Route>
         </Switch>
     </div>
     </Router>
-    </>
+    </div>
   );
 }
 
---
+++ client/src/components/user/UserMain.jsx
@@ -0,0 +1,9 @@
+const UserMain = ()=>{
+    return(
+        <div>
+            <h1>UserMain</h1>
+        </div>
+
+    );
+}
+export default UserMain;
